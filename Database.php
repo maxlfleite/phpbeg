e82192a9d15a106e20e1776b0b0ea5c01f24ad9c
Max Leite <maxlfleite@gmail.com>
2023-12-15 16:48:07 -0300

    Update Database.php

--- Database.php
+++ Database.php
@@ -2,7 +2,7 @@
 class Database {
     public $connection;
     public function __construct() {
-        $dsn = "mysql:host=localhost;port=3306;dbname=phpbeg;user=root;charset=utf8mb4";
+        $dsn = "mysql:host=localhost;port=3306;dbname=maxdatabase;user=root;charset=utf8mb4";
 
         $this->connection = new PDO($dsn);
     }
